c0e0e1570cd114d1ebfdf2003dd7ea4f21c06535
mjcardoza <68401098+mjcardoza@users.noreply.github.com>
2023-06-13 10:37:45 -0400

    Updated
    Now the random name and random number generate on unique unordered lists

--- index.js
+++ index.js
@@ -1,41 +1,6 @@
-/* const gmNames = ["Malcolm", "Andrew", "Bradley", "Rob", "Scott", "Canoe", "Kris", "Cole", "Luke", "Jesse", "Mark", "NewGM"]; */
-/* let randomName = Math.floor(Math.random()* gmNames.length);
-
-function getName(){
-    console.log(gmNames[randomName])
-} */
-
-
-/* function shuffle(gmNames){
-    let length = gmNames.length, t, i;
-    while (m){
-    i = Math.floor(Math.random()* m--)
-
-    t = gmNames[m]
-    gmNames[m] = gmNames[i]
-    gmNames [i] = t
-}
-return gmNames
-}
- */
-/* [... new Set(randomName)]
-let listEl = document.getElementById("list-el")
- */
-/* gmNames.forEach((item) => {
-    let li = document.createElement("li")
-    li.innerText = item
-    listEl.appendChild(li)
-})
- */
-/* document.getElementById("gm").onclick = getName() */
-
-/* gmNames.sort(() => Math.random() - 0.5)
-console.log(gmNames)
- */
-
 const names = ["Malcolm", "Andrew", "Bradley", "Rob", "Scott", "Canoe", "Kris", "Cole", "Luke", "Jesse", "Mark", "NewGM"];
 
-/* subheading: Add name to list */
+/* SUBHEADING: ADD NAME TO THE LIST */ 
 
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -59,47 +24,58 @@ function getNextName() {
   return name;
 }
 
-const list = document.querySelector('#names-list');
-const randomNumbersList = document.querySelector('#random-numbers-list')
+const namesList = document.querySelector('#names-list')
+const numbersList = document.createElement('ul');
+document.body.appendChild(numbersList);
 
 function addNameToList() {
     const name = getNextName();
     if (name) {
       const listItem = document.createElement('li');
       listItem.textContent = name;
-      list.appendChild(listItem);
+      namesList.appendChild(listItem);
+
+
+      const numberListItem = document.createElement('li')
+      numberListItem.textContent = randomNumber
+      numbersList.appendChild(numberListItem)
   }
 }
-
-
-function getRandomNumbers() {
-    const numbers = Array.from({ length: 12 }, (_, i) => i + 1);
-    const randomNumbers = [];
-  
-    while (randomNumbers.length < 12) {
-      const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
-      if (!randomNumbers.includes(randomNumber)) {
-        randomNumbers.push(randomNumber);
-      }
-    }
-    return randomNumbers;
-  }
-
-function addNumberToList(){
-    const randomNumbersListItem = document.createElement('li')
-      const randomNum = Math.floor(Math.random()* 12) + 1
-      randomNumbersListItem.textContent = randomNum
-      randomNumbersList.appendChild(randomNumbersListItem)
-
-}
-
-/* subheading: add number to name in following list */
 
   const gmButton = document.querySelector('#getGm-Btn');
 
   gmButton.addEventListener('click', addNameToList);
 
+  const selectButton = document.querySelector("#selector-Btn")
 
-  const selectButton = document.querySelector('#selectGm-Btn')
-  gmButton.addEventListener('click', addNameToList);
+/* SUBHEADING: ADD NUMBER TO NAME USING THE 'SELECT ORDER' BUTTON */
 
+
+const numbers = Array.from({ length: 12 }, (_, i) => i + 1);
+const randomNumbers = [];
+
+while (randomNumbers.length < 12) {
+  const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  if (!randomNumbers.includes(randomNumber)) {
+    randomNumbers.push(randomNumber);
+  }
+}
+
+const button = document.createElement('button');
+button.textContent = 'Generate Random Number';
+document.body.appendChild(button);
+
+let i = 0;
+function displayRandomNumber() {
+  const listItem = document.createElement('li');
+  listItem.textContent = randomNumbers[i];
+  numbersList.appendChild(listItem);
+  
+  i++;
+  
+  if (i < randomNumbers.length) {
+    button.addEventListener('click', displayRandomNumber);
+  }
+}
+
+button.addEventListener('click', displayRandomNumber);
